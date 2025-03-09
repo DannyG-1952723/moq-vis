758f852da4d71e1db4374d8eb7819c7018734b5e
Danny Grispen <danny.grispen@student.uhasselt.be>
2025-03-09 20:29:42 +0100

    Fixed IconButton width

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -6,7 +6,7 @@ interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export default function IconButton({ icon, ...props }: IconButtonProps) {
     return (
-        <button {...props} className="cursor-pointer text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+        <button {...props} className="cursor-pointer text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm min-w-8 min-h-8 w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
             {icon}
         </button>
     );

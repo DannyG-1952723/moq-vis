3863df22ae25a7f3c8506dd7fe784b826df75279
Danny Grispen <danny.grispen@student.uhasselt.be>
2025-03-10 13:58:10 +0100

    Made InlineCode inherit text size

--- src/components/InlineCode.tsx
+++ src/components/InlineCode.tsx
@@ -1,9 +1,7 @@
-// TODO: Add styling
-
 interface InlineCodeProps {
     children: string;
 }
 
 export default function InlineCode({ children }: InlineCodeProps) {
-    return <code className="font-mono text-sm border border-gray-200 rounded-md text-gray-500 bg-gray-50 p-0.5">{children}</code>;
+    return <code className="font-mono text-[length:inherit] border border-gray-200 rounded-md text-gray-500 bg-gray-50 p-0.5">{children}</code>;
 }

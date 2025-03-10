c06754756bd1cf8ec27f3356b04deb84840ee932
Danny Grispen <danny.grispen@student.uhasselt.be>
2025-03-10 14:16:12 +0100

    Added a note when the file list is empty

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,6 +1,7 @@
 import { LogFile } from "@/model/LogFile";
 import FileDetails from "./FileDetails";
 import { MouseEvent } from "react";
+import Note from "./Note";
 
 interface FileListProps {
     files: LogFile[];
@@ -9,11 +10,12 @@ interface FileListProps {
 
 export default function FileList({ files, handleDelete }: FileListProps) {
     const fileElements = files.map((file) => <FileDetails key={file.name} file={file} handleDelete={(event: MouseEvent<HTMLButtonElement>) => handleDelete(event, file.name)} />);
+    const note = <Note>No files have been imported</Note>;
 
     return (
         <>
             <h3 className="block mt-5 mb-2 text-md font-medium text-gray-900 dark:text-white">Imported files</h3>
-            <div className="flex flex-wrap mt-4">{fileElements}</div>
+            <div className="flex flex-wrap">{files.length > 0 ? fileElements : note}</div>
         </>
     );
 }

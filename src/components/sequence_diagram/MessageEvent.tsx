f5d48a26c9d0aed30440004faa21e88f5b421243
Danny Grispen <danny.grispen@student.uhasselt.be>
2025-03-24 22:06:22 +0100

    Added a background to the messege event label

--- src/components/sequence_diagram/MessageEvent.tsx
+++ src/components/sequence_diagram/MessageEvent.tsx
@@ -1,4 +1,5 @@
 import { ConnectionEvent } from "@/model/Network";
+import { useEffect, useRef } from "react";
 
 interface MessageEventProps {
     createdEvent: ConnectionEvent;
@@ -10,8 +11,30 @@ interface MessageEventProps {
 
 const lineOffset = 8;
 const arrowOffset = 15;
+const textBgPaddingX = 6;
+const textBgPaddingY = 4;
 
 export default function MessageEvent({createdEvent, parsedEvent, blockSize, xScale, yScale}: MessageEventProps) {
+    const textRef = useRef<SVGTextElement>(null);
+    const textBgRef = useRef<SVGRectElement>(null);
+
+    useEffect(() => {
+        if (!textRef.current || !textBgRef.current) {
+            console.log("textRef is null");
+            return;
+        }
+
+        const boundingBox = textRef.current.getBBox();
+
+        textRef.current.setAttribute("transform", `rotate(${textAngle}, ${textMiddleX}, ${textMiddleY}) translate(${0}, ${boundingBox.height / 4})`);
+
+        textBgRef.current.setAttribute("x", `${boundingBox.x - textBgPaddingX}`);
+        textBgRef.current.setAttribute("y", `${boundingBox.y - textBgPaddingY}`);
+        textBgRef.current.setAttribute("width", `${boundingBox.width + 2 * textBgPaddingX}`);
+        textBgRef.current.setAttribute("height", `${boundingBox.height + 2 * textBgPaddingY}`);
+        textBgRef.current.setAttribute("transform", `rotate(${textAngle}, ${textMiddleX}, ${textMiddleY}) translate(${0}, ${boundingBox.height / 4})`);
+    }, []);
+
     const createdX = xScale(createdEvent.fileName)!;
     const createdY = yScale(createdEvent.eventNum);
     const parsedX = xScale(parsedEvent.fileName)!;
@@ -41,7 +64,8 @@ export default function MessageEvent({createdEvent, parsedEvent, blockSize, xSca
             <rect transform={`translate(${createdX}, ${createdY})`} y={-15} width={blockSize} height={blockSize} rx={5} />
             <rect transform={`translate(${parsedX}, ${parsedY})`} y={-15} width={blockSize} height={blockSize} rx={5} />
             <line x1={arrowStartX} y1={arrowStartY} x2={arrowEndX} y2={arrowEndY} stroke="black" strokeWidth={3} markerEnd="url(#arrow)" strokeLinecap="round" />
-            <text transform={`rotate(${textAngle}, ${textMiddleX}, ${textMiddleY})`} x={textMiddleX} y={textMiddleY} style={{textAnchor: "middle"}}>{shortName}</text>
+            <rect ref={textBgRef} rx={5} fill="black" />
+            <text ref={textRef} transform={`rotate(${textAngle}, ${textMiddleX}, ${textMiddleY})`} x={textMiddleX} y={textMiddleY} style={{textAnchor: "middle"}} fill="white">{shortName}</text>
         </>
     );
 

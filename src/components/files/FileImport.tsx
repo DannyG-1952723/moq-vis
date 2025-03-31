d5e3f2df550be93757b6aaf70c3a63f4d00bb333
Danny Grispen <danny.grispen@student.uhasselt.be>
2025-03-31 02:30:53 +0200

    Made loading of demo files manual since GitHub pages doesn't support Nextjs API

--- src/components/files/FileImport.tsx
+++ src/components/files/FileImport.tsx
@@ -13,6 +13,8 @@ export default function FileImport() {
     const files = useFiles();
     const dispatch = useFilesDispatch();
 
+    const demoLogs = ["1_clock_pub.sqlog", "2_relay.sqlog", "3_clock_sub.sqlog"];
+
     return (
         <form className="w-full">
             <FileInput handleImport={handleImport} />
@@ -36,12 +38,22 @@ export default function FileImport() {
     function handleDemoImport(event: MouseEvent<HTMLButtonElement>) {
         event.preventDefault();
 
-        fetch("/api/getLogs")
-            .then((res) => res.json())
-            .then((data) => {
-                const importedFiles = data.logs.map((file: { name: string, content: string }) => new File([file.content], file.name));
-                handleFileImport(importedFiles);
-            });
+        const fetchLogs = async () => {
+            const fetchedLogs = await Promise.all(
+                demoLogs.map(async (file) => {
+                    const response = await fetch(`/demo_logs/${file}`);
+                    const buffer = await response.arrayBuffer();
+                    const content = new TextDecoder().decode(new Uint8Array(buffer));
+                    return { name: file, content };
+                })
+            );
+
+            const fileList = fetchedLogs.map((file: {name: string, content: string}) => new File([file.content], file.name));
+
+            handleFileImport(fileList as unknown as FileList);
+        }
+
+        fetchLogs();
     }
 
     function handleFileImport(importedFiles: FileList) {

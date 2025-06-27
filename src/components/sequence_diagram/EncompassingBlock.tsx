c1b5d21e56fb4b26166ef35caecb5c60dabd681f
Danny Grispen <danny.grispen@student.uhasselt.be>
2025-06-28 00:26:59 +0200

    Added text and arrows to the message event blocks

--- src/components/sequence_diagram/EncompassingBlock.tsx
+++ src/components/sequence_diagram/EncompassingBlock.tsx
@@ -6,6 +6,8 @@ import Modal from "@/components/Modal";
 import { MessageEvent } from "@/model/Events";
 
 const SPACING = 5;
+const ARROW_WIDTH = 20;
+const ARROW_WIDTH_HOVER = 22;
 const BORDER_WIDTH = 4;
 const BORDER_WIDTH_HOVER = 6;
 
@@ -25,12 +27,25 @@ export default function EncompassingBlock({ createdEvent, parsedEvent, x1, y1, x
     const [fillColor, setFillColor] = useState(colors.transparentNormal);
     const [borderColor, setBorderColor] = useState(colors.normal);
     const [borderWidth, setBorderWidth] = useState(BORDER_WIDTH);
+    const [arrowWidth, setArrowWidth] = useState(ARROW_WIDTH);
     const [showModal, setShowModal] = useState(false);
 
     const topLeftX = (x1 < x2 ? x1 : x2) + BLOCK_SIZE + SPACING + borderWidth / 2;
     const topLeftY = (y1 < y2 ? y1 - BLOCK_SIZE / 2 : y2) + borderWidth / 2;
     const bottomRightX = ((x1 < x2 ? x2 : x1) - SPACING) - borderWidth / 2;
     const bottomRightY = (y1 < y2 ? y2 + BLOCK_SIZE / 2 : y1) - borderWidth / 2;
+
+    const textMiddleX = topLeftX + (bottomRightX - topLeftX) / 2;
+    const textY = topLeftY + 25;
+
+    const arrowLeftSide = textMiddleX - arrowWidth / 2;
+    const arrowRightSide = arrowLeftSide + arrowWidth;
+    const arrowPointX = (x1 < x2) ? arrowRightSide : arrowLeftSide;
+    const arrowSideX = (x1 < x2) ? arrowLeftSide : arrowRightSide;
+    const topArrowTopY = topLeftY - arrowWidth / 2;
+    const topArrowBottomY = topArrowTopY + arrowWidth;
+    const bottomArrowTopY = bottomRightY - arrowWidth / 2;
+    const bottomArrowBottomY = bottomArrowTopY + arrowWidth;
 
     const shortName = createdEvent.event.getShortNameWithoutAction();
 
@@ -40,6 +55,9 @@ export default function EncompassingBlock({ createdEvent, parsedEvent, x1, y1, x
         <>
             <g onClick={_ => setShowModal(true)} className="cursor-pointer" onMouseEnter={_ => handleMouseEnter()} onMouseLeave={_ => handleMouseLeave()}>
                 <rect x={topLeftX} y={topLeftY} width={bottomRightX - topLeftX} height={bottomRightY - topLeftY} rx={5} fill={fillColor} stroke={borderColor} strokeWidth={borderWidth} />
+                <polygon points={`${arrowPointX},${topLeftY} ${arrowSideX},${topArrowTopY} ${arrowSideX},${topArrowBottomY}`} fill={borderColor} />
+                <polygon points={`${arrowPointX},${bottomRightY} ${arrowSideX},${bottomArrowTopY} ${arrowSideX},${bottomArrowBottomY}`} fill={borderColor} />
+                <text x={textMiddleX} y={textY} textAnchor="middle" >{shortName}</text>
             </g>
 
             {/* TODO: Merge the content of both events in the modal code block */}
@@ -54,6 +72,7 @@ export default function EncompassingBlock({ createdEvent, parsedEvent, x1, y1, x
         setFillColor(colors.transparentHover);
         setBorderColor(colors.hover);
         setBorderWidth(BORDER_WIDTH_HOVER);
+        setArrowWidth(ARROW_WIDTH_HOVER);
     }
 
     function handleMouseLeave() {
@@ -61,5 +80,6 @@ export default function EncompassingBlock({ createdEvent, parsedEvent, x1, y1, x
         setFillColor(colors.transparentNormal);
         setBorderColor(colors.normal);
         setBorderWidth(BORDER_WIDTH);
+        setArrowWidth(ARROW_WIDTH);
     }
 }

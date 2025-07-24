5e65791d24830f0e1dc775f0cf5f2530658eb6d7
Danny Grispen <danny.grispen@student.uhasselt.be>
2025-07-24 20:47:12 +0200

    Added zooming and panning to the charts

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -3,7 +3,7 @@ import * as d3 from "d3";
 import AxisBottom from "./AxisBottom";
 import AxisLeft from "./AxisLeft";
 import Marks from "./Marks";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const MARGIN = { top: 60, bottom: 50, left: 65, right: 50 };
 const WIDTH = 624;
@@ -20,7 +20,28 @@ interface ChartProps {
 }
 
 export default function Chart({ showQuicData, showMoqData, quicConnection, moqConnection }: ChartProps) {
+    const chartRef = useRef<SVGGElement>(null);
     const popupContainerRef = useRef<SVGGElement>(null);
+    const [currentZoomState, setCurrentZoomState] = useState<d3.ZoomTransform>();
+
+    useEffect(() => {
+        if (!chartRef) {
+            return;
+        }
+
+        const chart = d3.select<SVGGElement, unknown>(chartRef.current!);
+
+        const zoomBehavior = d3.zoom<SVGGElement, unknown>()
+            .scaleExtent([1, 32])
+            .translateExtent([[0, 0], [INNER_WIDTH, INNER_HEIGHT]])
+            .extent([[0, 0], [INNER_WIDTH, INNER_HEIGHT]])
+            .on("zoom", () => {
+                const zoomState = d3.zoomTransform(chart.node()!);
+                setCurrentZoomState(zoomState);
+            });
+
+        chart.call(zoomBehavior);
+    }, [currentZoomState]);
 
     const quicTimestamps = quicConnection?.messageEvents.map(event => event.createdEvent.event.time) ?? [];
     const moqTimestamps = moqConnection?.messageEvents.map(event => event.createdEvent.event.time) ?? [];
@@ -43,10 +64,14 @@ export default function Chart({ showQuicData, showMoqData, quicConnection, moqCo
         .domain([minNormalizedTimestamp, maxNormalizedTimestamp])
         .range([0, INNER_WIDTH])
         .nice();
+    
+    if (currentZoomState) {
+        xScale.domain(currentZoomState.rescaleX(xScale).domain());
+    }
 
     const yScale = d3.scaleLinear()
-        .domain([maxLatency, minLatency])
-        .range([0, INNER_HEIGHT])
+        .domain([minLatency, maxLatency])
+        .range([INNER_HEIGHT, 0])
         .nice();
 
     const startingConnFileName = quicConnection === undefined ? moqConnection!.startingConn.fileName : quicConnection.startingConn.fileName;
@@ -64,12 +89,28 @@ export default function Chart({ showQuicData, showMoqData, quicConnection, moqCo
 
     return (
         <svg className="flex-shrink-0 mb-5" width={WIDTH} height={HEIGHT}>
+            <defs>
+                {/* Prevents the chart to go out of bounds when zooming and panning (covers the left axis otherwise) */}
+                <clipPath id="chart-clip">
+                    {/* Make the clip path a bit bigger to not cut off parts of the points */}
+                    <rect x={-5} y={-5} width={INNER_WIDTH + 10} height={INNER_HEIGHT + 10} />
+                </clipPath>
+            </defs>
+
             <text fontSize="1.2em" x={WIDTH / 2} y={30} textAnchor="middle">{`Connection ${startingConnFileName} \u2013 ${acceptingConnFileName}`}</text>
-            <g transform={`translate(${MARGIN.left}, ${MARGIN.top})`}>
-                <AxisBottom xScale={xScale} width={INNER_WIDTH} height={INNER_HEIGHT} />
+
+            <g ref={chartRef} transform={`translate(${MARGIN.left}, ${MARGIN.top})`}>
+                {/* Needed so zooming and dragging works on the background and not only the visible elements */}
+                <rect width={INNER_WIDTH} height={INNER_HEIGHT} fill="transparent" />
+
+                <g clipPath="url(#chart-clip)">
+                    {quicMarks}
+                    {moqMarks}
+                </g>
+
                 <AxisLeft yScale={yScale} height={INNER_HEIGHT} />
-                {quicMarks}
-                {moqMarks}
+                <AxisBottom xScale={xScale} width={INNER_WIDTH} height={INNER_HEIGHT} />
+
                 <g ref={popupContainerRef} className="popup-container" />
             </g>
         </svg>
